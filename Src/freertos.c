65c57e1d2e7e82610183310521e77bf384bdeabf
Krzysztof Pogoda <zinith.kp@gmail.com>
2025-06-02 14:27:22 +0200

    Updated freertos tasks for pid calculations

--- Src/freertos.c
+++ Src/freertos.c
@@ -60,72 +60,86 @@
 /* Definitions for task_encodersUpdate */
 osThreadId_t task_encodersUpdateHandle;
 const osThreadAttr_t task_encodersUpdate_attributes = {
-  .name = "task_encodersUpdate",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityAboveNormal,
+    .name = "task_encodersUpdate",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
 /* Definitions for task_stateLEDUpdate */
 osThreadId_t task_stateLEDUpdateHandle;
 const osThreadAttr_t task_stateLEDUpdate_attributes = {
-  .name = "task_stateLEDUpdate",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "task_stateLEDUpdate",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for task_pollModbus */
 osThreadId_t task_pollModbusHandle;
 const osThreadAttr_t task_pollModbus_attributes = {
-  .name = "task_pollModbus",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityBelowNormal,
+    .name = "task_pollModbus",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityBelowNormal,
 };
 /* Definitions for task_checkIfTimeout */
 osThreadId_t task_checkIfTimeoutHandle;
 const osThreadAttr_t task_checkIfTimeout_attributes = {
-  .name = "task_checkIfTimeout",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+    .name = "task_checkIfTimeout",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityLow,
 };
-/* Definitions for task_PIDCalculate_Rot */
-osThreadId_t task_PIDCalculate_RotHandle;
-const osThreadAttr_t task_PIDCalculate_Rot_attributes = {
-  .name = "task_PIDCalculate_Rot",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityHigh,
+/* Definitions for task_PID_Rot_Position */
+osThreadId_t task_PID_Rot_PositionHandle;
+const osThreadAttr_t task_PID_Rot_Position_attributes = {
+    .name = "task_PID_Rot_Position",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
-/* Definitions for task_PIDCalculate_Elev */
-osThreadId_t task_PIDCalculate_ElevHandle;
-const osThreadAttr_t task_PIDCalculate_Elev_attributes = {
-  .name = "task_PIDCalculate_Elev",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityHigh,
+/* Definitions for task_PID_Elev_Position */
+osThreadId_t task_PID_Elev_PositionHandle;
+const osThreadAttr_t task_PID_Elev_Position_attributes = {
+    .name = "task_PID_Elev_Position",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
 /* Definitions for task_debugUSBPrint */
 osThreadId_t task_debugUSBPrintHandle;
 const osThreadAttr_t task_debugUSBPrint_attributes = {
-  .name = "task_debugUSBPrint",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+    .name = "task_debugUSBPrint",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityLow,
 };
 /* Definitions for task_checkAccelValues */
 osThreadId_t task_checkAccelValuesHandle;
 const osThreadAttr_t task_checkAccelValues_attributes = {
-  .name = "task_checkAccelValues",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityAboveNormal,
+    .name = "task_checkAccelValues",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
 /* Definitions for task_checkAnalogSensorsData */
 osThreadId_t task_checkAnalogSensorsDataHandle;
 const osThreadAttr_t task_checkAnalogSensorsData_attributes = {
-  .name = "task_checkAnalogSensorsData",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityAboveNormal,
+    .name = "task_checkAnalogSensorsData",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
 /* Definitions for task_shootingTimeLimit */
 osThreadId_t task_shootingTimeLimitHandle;
 const osThreadAttr_t task_shootingTimeLimit_attributes = {
-  .name = "task_shootingTimeLimit",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityHigh7,
+    .name = "task_shootingTimeLimit",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityHigh7,
+};
+/* Definitions for task_PID_Rot_Speed */
+osThreadId_t task_PID_Rot_SpeedHandle;
+const osThreadAttr_t task_PID_Rot_Speed_attributes = {
+    .name = "task_PID_Rot_Speed",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
+};
+/* Definitions for task_PID_Elev_Speed */
+osThreadId_t task_PID_Elev_SpeedHandle;
+const osThreadAttr_t task_PID_Elev_Speed_attributes = {
+    .name = "task_PID_Elev_Speed",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityAboveNormal,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -137,22 +151,25 @@ void taskInit_encodersUpdate(void *argument);
 void taskInit_stateLEDUpdate(void *argument);
 void taskInit_pollModbus(void *argument);
 void taskInit_checkIfTimeout(void *argument);
-void taskInit_PIDCalculate_Rot(void *argument);
-void taskInit_PIDCalculate_Elev(void *argument);
+void taskInit_PID_Rot_Position(void *argument);
+void taskInit_PID_Elev_Position(void *argument);
 void taskInit_debugUSBPrint(void *argument);
 void taskInit_checkAccelValues(void *argument);
 void taskInit_checkAnalogSensorsData(void *argument);
 void taskInit_shootingTimeLimit(void *argument);
+void taskInit_PID_Rot_Speed(void *argument);
+void taskInit_PID_Elev_Speed(void *argument);
 
 extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
-void MX_FREERTOS_Init(void) {
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
+void MX_FREERTOS_Init(void)
+{
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
@@ -186,11 +203,11 @@ void MX_FREERTOS_Init(void) {
   /* creation of task_checkIfTimeout */
   task_checkIfTimeoutHandle = osThreadNew(taskInit_checkIfTimeout, NULL, &task_checkIfTimeout_attributes);
 
-  /* creation of task_PIDCalculate_Rot */
-  task_PIDCalculate_RotHandle = osThreadNew(taskInit_PIDCalculate_Rot, NULL, &task_PIDCalculate_Rot_attributes);
+  /* creation of task_PID_Rot_Position */
+  task_PID_Rot_PositionHandle = osThreadNew(taskInit_PID_Rot_Position, NULL, &task_PID_Rot_Position_attributes);
 
-  /* creation of task_PIDCalculate_Elev */
-  task_PIDCalculate_ElevHandle = osThreadNew(taskInit_PIDCalculate_Elev, NULL, &task_PIDCalculate_Elev_attributes);
+  /* creation of task_PID_Elev_Position */
+  task_PID_Elev_PositionHandle = osThreadNew(taskInit_PID_Elev_Position, NULL, &task_PID_Elev_Position_attributes);
 
   /* creation of task_debugUSBPrint */
   task_debugUSBPrintHandle = osThreadNew(taskInit_debugUSBPrint, NULL, &task_debugUSBPrint_attributes);
@@ -204,6 +221,12 @@ void MX_FREERTOS_Init(void) {
   /* creation of task_shootingTimeLimit */
   task_shootingTimeLimitHandle = osThreadNew(taskInit_shootingTimeLimit, NULL, &task_shootingTimeLimit_attributes);
 
+  /* creation of task_PID_Rot_Speed */
+  task_PID_Rot_SpeedHandle = osThreadNew(taskInit_PID_Rot_Speed, NULL, &task_PID_Rot_Speed_attributes);
+
+  /* creation of task_PID_Elev_Speed */
+  task_PID_Elev_SpeedHandle = osThreadNew(taskInit_PID_Elev_Speed, NULL, &task_PID_Elev_Speed_attributes);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -211,7 +234,6 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_EVENTS */
   /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
-
 }
 
 /* USER CODE BEGIN Header_taskInit_encodersUpdate */
@@ -232,10 +254,8 @@ void taskInit_encodersUpdate(void *argument)
   /* Infinite loop */
   for (;;)
   {
-    if (encoderUpdate((Encoder *)&rot_motor_encoder))
-      vTaskResume(task_PIDCalculate_RotHandle);
-    if (encoderUpdate((Encoder *)&elev_motor_encoder))
-      vTaskResume(task_PIDCalculate_ElevHandle);
+    encoderUpdate((Encoder *)&rot_motor_encoder);
+    encoderUpdate((Encoder *)&elev_motor_encoder);
     osDelay(ENCODER_SAMPLING_TIME_MS / portTICK_RATE_MS);
   }
   /* USER CODE END taskInit_encodersUpdate */
@@ -298,40 +318,48 @@ void taskInit_checkIfTimeout(void *argument)
   /* USER CODE END taskInit_checkIfTimeout */
 }
 
-/* USER CODE BEGIN Header_taskInit_PIDCalculate_Rot */
+/* USER CODE BEGIN Header_taskInit_PID_Rot_Position */
 /**
- * @brief Function implementing the task_PIDCalculate_Rot thread.
+ * @brief Function implementing the task_PID_Rot_Position thread.
  * @param argument: Not used
  * @retval None
  */
-/* USER CODE END Header_taskInit_PIDCalculate_Rot */
-void taskInit_PIDCalculate_Rot(void *argument)
+/* USER CODE END Header_taskInit_PID_Rot_Position */
+void taskInit_PID_Rot_Position(void *argument)
 {
-  /* USER CODE BEGIN taskInit_PIDCalculate_Rot */
+  /* USER CODE BEGIN taskInit_PID_Rot_Position */
   /* Infinite loop */
   for (;;)
   {
-    vTaskSuspend(NULL);
+    osDelay(1); // delay 55 ms at the beginning of loop, read delay from macro defined in pid.h
+    // here copy feedback position from REG_INPUT_ROT_POSITION to pid struct PV
+    // here copy target position from REG_HOLDING_ROT_TARGET_POSITION to pid struct SP
+    // here call pidUpdate() function
+    // here copy from pid struct CV to speed pid struct CV
   }
-  /* USER CODE END taskInit_PIDCalculate_Rot */
+  /* USER CODE END taskInit_PID_Rot_Position */
 }
 
-/* USER CODE BEGIN Header_taskInit_PIDCalculate_Elev */
+/* USER CODE BEGIN Header_taskInit_PID_Elev_Position */
 /**
- * @brief Function implementing the task_PIDCalculate_Elev thread.
+ * @brief Function implementing the task_PID_Elev_Position thread.
  * @param argument: Not used
  * @retval None
  */
-/* USER CODE END Header_taskInit_PIDCalculate_Elev */
-void taskInit_PIDCalculate_Elev(void *argument)
+/* USER CODE END Header_taskInit_PID_Elev_Position */
+void taskInit_PID_Elev_Position(void *argument)
 {
-  /* USER CODE BEGIN taskInit_PIDCalculate_Elev */
+  /* USER CODE BEGIN taskInit_PID_Elev_Position */
   /* Infinite loop */
   for (;;)
   {
-    vTaskSuspend(NULL);
+    osDelay(1); // delay 55 ms at the beginning of loop, read delay from macro defined in pid.h
+    // here copy feedback position from REG_INPUT_ELEV_POSITION to pid struct PV
+    // here copy target position from REG_HOLDING_ELEV_TARGET_POSITION to pid struct SP
+    // here call pidUpdate() function
+    // here copy from pid struct CV to speed pid struct CV
   }
-  /* USER CODE END taskInit_PIDCalculate_Elev */
+  /* USER CODE END taskInit_PID_Elev_Position */
 }
 
 /* USER CODE BEGIN Header_taskInit_debugUSBPrint */
@@ -418,8 +446,53 @@ void taskInit_shootingTimeLimit(void *argument)
   /* USER CODE END taskInit_shootingTimeLimit */
 }
 
+/* USER CODE BEGIN Header_taskInit_PID_Rot_Speed */
+/**
+ * @brief Function implementing the task_PID_Rot_Speed thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_taskInit_PID_Rot_Speed */
+void taskInit_PID_Rot_Speed(void *argument)
+{
+  /* USER CODE BEGIN taskInit_PID_Rot_Speed */
+  /* Infinite loop */
+  for (;;)
+  {
+    osDelay(1); // delay 25 ms at the beginning of loop, read delay from macro defined in pid.h
+    // here copy feedback position from REG_INPUT_ROT_SPEED to pid struct PV
+    // IF PID MODE IS SPEED ONLY, here copy target speed from REG_HOLDING_ROT_TARGET_SPEED to pid struct SP
+    // ELSE sp is set in another task, so do nothing :)
+    // here call pidUpdate() function
+    // here I will copy data from pid struct CV to correct medium (pwm or uart)
+  }
+  /* USER CODE END taskInit_PID_Rot_Speed */
+}
+
+/* USER CODE BEGIN Header_taskInit_PID_Elev_Speed */
+/**
+ * @brief Function implementing the task_PID_Elev_Speed thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_taskInit_PID_Elev_Speed */
+void taskInit_PID_Elev_Speed(void *argument)
+{
+  /* USER CODE BEGIN taskInit_PID_Elev_Speed */
+  /* Infinite loop */
+  for (;;)
+  {
+    osDelay(1); // delay 25 ms at the beginning of loop, read delay from macro defined in pid.h
+    // here copy feedback position from REG_INPUT_ELEV_SPEED to pid struct PV
+    // IF PID MODE IS SPEED ONLY, here copy target speed from REG_HOLDING_ELEV_TARGET_SPEED to pid struct SP
+    // ELSE sp is set in another task, so do nothing :)
+    // here call pidUpdate() function
+    // here I will copy data from pid struct CV to correct medium (pwm or uart)
+  }
+  /* USER CODE END taskInit_PID_Elev_Speed */
+}
+
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
-
