d0649e392ed771369c37d865f2700454155e6f08
stakrask <kraskiewicz.s@gmail.com>
2025-06-18 12:52:04 +0200

    added conversion of units to analog sensors

--- Inc/Sensors/analog_sensors.h
+++ Inc/Sensors/analog_sensors.h
@@ -11,15 +11,22 @@
 #define CURRENT_SENSOR_OFFSET   1.65f  
 #define CURRENT_SENSOR_SENSITIVITY 0.066f 
 
+// Stałe ADC
+#define ADC_RESOLUTION_BITS     12
+#define ADC_MAX_VALUE          ((1 << ADC_RESOLUTION_BITS) - 1)  // 4095 dla 12-bit
+#define ADC_VREF               3.3f  // Napięcie referencyjne w V
+
+// Stałe konwersji prądu
+#define CURRENT_OFFSET_BEFORE_CONVERSION  3102  // Offset w jednostkach ADC (-5A)
+#define CURRENT_OFFSET_AFTER_CONVERSION   5000  // Offset w mA (-5A)
+
 // Deklaracje funkcji
 void Start_ADC_Conversion(void);
-
-/**
- * @brief Aktualizuje rejestry wejściowe Modbus wartościami odczytanymi z czujników analogowych
- */
-void analogSensorsUpdate();
-
-// Deklaracja callback'a dla konwersji ADC
+void analogSensorsUpdate(void);
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc);
+
+// Funkcje konwersji
+int16_t convertCurrentToMilliamps(uint16_t adc_value);
+uint16_t convertVoltageToMillivolts(uint16_t adc_value);
 
 #endif /* ANALOG_SENSORS_H */
--- Src/Sensors/analog_sensors.c
+++ Src/Sensors/analog_sensors.c
@@ -26,36 +26,86 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 }
 
 /**
+ * @brief Konwertuje wartość ADC na prąd w miliamperach
+ * @param adc_value Surowa wartość z ADC (0-4095)
+ * @return Prąd w miliamperach (ze znakiem, zakres zależy od czujnika)
+ * 
+ *  Odejmowanie offsetu PRZED konwersją
+ * - Odejmuje offset 3102 (odpowiednik -5A) od surowej wartości ADC
+ * - Następnie konwertuje na miliampery
+ */
+int16_t convertCurrentToMilliamps(uint16_t adc_value)
+{
+    // Odejmij offset przed konwersją
+    int32_t adc_with_offset = (int32_t)adc_value - CURRENT_OFFSET_BEFORE_CONVERSION;
+    
+    // Konwertuj na napięcie (V)
+    float voltage = ((float)adc_with_offset * ADC_VREF) / ADC_MAX_VALUE;
+    
+    // Konwertuj na prąd (A) używając czułości czujnika
+    // I = (V - V_offset) / Sensitivity
+    float current_amps = (voltage - CURRENT_SENSOR_OFFSET) / CURRENT_SENSOR_SENSITIVITY;
+    
+    // Konwertuj na miliampery
+    int16_t current_ma = (int16_t)(current_amps * 1000.0f);
+    
+    return current_ma;
+}
+
+/**
+ * @brief Konwertuje wartość ADC na napięcie w miliowoltach
+ * @param adc_value Surowa wartość z ADC (0-4095)
+ * @return Napięcie w miliowoltach (bez offsetu)
+ */
+uint16_t convertVoltageToMillivolts(uint16_t adc_value)
+{
+    // Konwertuj na napięcie przed dzielnikiem (V)
+    float voltage_before_divider = ((float)adc_value * ADC_VREF) / ADC_MAX_VALUE;
+    
+    // Uwzględnij dzielnik napięcia
+    float actual_voltage = voltage_before_divider * VOLTAGE_DIVIDER_RATIO;
+    
+    // Konwertuj na miliowolty
+    uint16_t voltage_mv = (uint16_t)(actual_voltage * 1000.0f);
+    
+    return voltage_mv;
+}
+
+/**
  * @brief Aktualizuje rejestry wejściowe Modbus wartościami odczytanymi z czujników analogowych
  * 
  * Ta funkcja powinna być wywoływana cyklicznie, aby zapewnić aktualne wartości w rejestrach.
  */
 void analogSensorsUpdate(void)
 {
-  static uint16_t local_adc_values[4] = {0}; // Lokalna kopia wartości ADC
-  
-  // Sprawdź, czy konwersja jest zakończona
-  if (adc_conversion_complete)
-  {
-    // Wykonaj lokalną kopię wartości ADC, aby uniknąć problemu z wyścigami
-    for (int i = 0; i < 4; i++)
+    static uint16_t local_adc_values[4] = {0}; // Lokalna kopia wartości ADC
+    
+    // Sprawdź, czy konwersja jest zakończona
+    if (adc_conversion_complete)
     {
-      local_adc_values[i] = adc_values[i];
+        // Wykonaj lokalną kopię wartości ADC, aby uniknąć problemu z wyścigami
+        for (int i = 0; i < 4; i++)
+        {
+            local_adc_values[i] = adc_values[i];
+        }
+        
+        // Konwertuj i przypisz wartości do rejestrów Modbus
+        
+        // Prądy - konwersja na miliampery (wybierz jedną z metod)
+        regInput[regInpIx(REG_INPUT_LAMP_CURRENT)] = convertCurrentToMilliamps(local_adc_values[0]);
+        regInput[regInpIx(REG_INPUT_RELOAD_CURRENT)] = convertCurrentToMilliamps(local_adc_values[1]);
+        regInput[regInpIx(REG_INPUT_TRIGGER_CURRENT)] = convertCurrentToMilliamps(local_adc_values[2]);
+        
+        // Napięcie - konwersja na miliowolty
+        regInput[regInpIx(REG_INPUT_BATTERY_VOLTAGE)] = convertVoltageToMillivolts(local_adc_values[3]);
+        
+        // Rozpocznij kolejną konwersję
+        Start_ADC_Conversion();
     }
-    
-    // Przypisz wartości do rejestrów Modbus
-    regInput[regInpIx(REG_INPUT_LAMP_CURRENT)] = local_adc_values[0];
-    regInput[regInpIx(REG_INPUT_RELOAD_CURRENT)] = local_adc_values[1];
-    regInput[regInpIx(REG_INPUT_TRIGGER_CURRENT)] = local_adc_values[2];
-    regInput[regInpIx(REG_INPUT_BATTERY_VOLTAGE)] = local_adc_values[3];
-    
-    // Rozpocznij kolejną konwersję
-    Start_ADC_Conversion();
-  }
-  else if (HAL_ADC_GetState(&hadc1) != HAL_ADC_STATE_BUSY_REG && 
-           HAL_ADC_GetState(&hadc1) != HAL_ADC_STATE_BUSY)
-  {
-    // Jeśli ADC nie jest zajęty, a flaga nie jest ustawiona, rozpocznij konwersję
-    Start_ADC_Conversion();
-  }
+    else if (HAL_ADC_GetState(&hadc1) != HAL_ADC_STATE_BUSY_REG && 
+             HAL_ADC_GetState(&hadc1) != HAL_ADC_STATE_BUSY)
+    {
+        // Jeśli ADC nie jest zajęty, a flaga nie jest ustawiona, rozpocznij konwersję
+        Start_ADC_Conversion();
+    }
 }
